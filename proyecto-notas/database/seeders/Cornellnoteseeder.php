6623702d4a2943e873ededa731684743ef42aef8
Marcolosada <marco937234@gmail.com>
2023-05-03 11:14:39 -0600

    se crearon los facorys y un seeder que hacia falta de igual manera

---
+++ proyecto-notas/database/seeders/Cornellnoteseeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Cornellnoteseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
